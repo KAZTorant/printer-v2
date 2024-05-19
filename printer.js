62f75fa3ac1a8fc81d0896ab4bc53e1779b9243e
Zakir Aghayev <agayevzakir239@gmail.com>
2024-05-19 17:57:59 +0400

    Style printer

--- printer.js
+++ printer.js
@@ -5,14 +5,11 @@ const escpos = require("escpos");
 const escposUSB = require("escpos-usb");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Read port from environment variable
 
 // Initialize the express-fileupload middleware
 app.use(fileUpload());
 
-// Set up your thermal printer with USB connection
-const device = new escposUSB();
-const printer = new escpos.Printer(device);
 
 const replacements = {
   'Ü': 'U',
@@ -54,7 +51,12 @@ app.post("/print", (req, res) => {
   // Convert the file content to a string in UTF-8 encoding
   const data = uploadedFile.data.toString("utf8");
 
-  const dataModified = replaceSymbols(data)
+  const dataModified = replaceSymbols(data);
+
+  // Set up your thermal printer with USB connection
+  const device = new escposUSB();
+  const printer = new escpos.Printer(device);
+
   // Send the text to the printer using the configured encoding
   device.open((err) => {
     if (err) {
@@ -70,6 +72,54 @@ app.post("/print", (req, res) => {
       .close();
       
     res.json({ message: "Printing started successfully" });
+  });
+});
+
+// API to accept and print text files line by line
+app.post("/print-line-by-line", (req, res) => {
+  // Ensure a file was uploaded
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  // Extract the uploaded file
+  const uploadedFile = req.files.textFile;
+
+  // Check if the uploaded file is a text file
+  if (uploadedFile.mimetype !== "text/plain") {
+    return res.status(400).json({ error: "Invalid file format. Please upload a .txt file" });
+  }
+
+  // Convert the file content to a string in UTF-8 encoding
+  const data = uploadedFile.data.toString("utf8");
+
+  const dataModified = replaceSymbols(data);
+  const lines = dataModified.split('\n');
+
+  // Set up your thermal printer with USB connection
+  const device = new escposUSB();
+  const printer = new escpos.Printer(device);
+  
+  // Send the text to the printer line by line with styling
+  device.open((err) => {
+    if (err) {
+      return res.status(500).json({ error: "Could not connect to printer" });
+    }
+
+    lines.forEach(line => {
+      printer
+        .font('a') // Use normal font for each line
+        .align('lt') // Left-aligned for each line
+        .style('normal') // Normal style for each line
+        .text(line)
+        .feed(1); // Add space after each line
+    });
+
+    printer
+      .cut()
+      .close();
+
+    res.json({ message: "Printing line by line started successfully" });
   });
 });
 
