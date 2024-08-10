089203580813b55e37a4bd3b358aea23d183fa49
zakiraghayev <57976785+zakiraghayev@users.noreply.github.com>
2024-08-11 01:19:14 +0400

    Update printer.js

--- printer.js
+++ printer.js
@@ -66,7 +66,7 @@ app.post("/print", (req, res) => {
       .align('lt') // Left-aligned
       .style('IU') // Normal style
       .text(dataModified)
-      .size(1,1)
+      .size(1,0)
       .cut()
       .close();
       
